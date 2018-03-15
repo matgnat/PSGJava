73bb0e7d2d22643cd1a937ba1b223312eecc7713
Mateusz Gnat <mateusz.gnat@mobica.com>
2018-03-16 00:43:04 +0100

    refactor validate and draw tree method

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.List;
+
 /**
  * Created by magn on 3/9/2018.
  */
@@ -9,7 +12,58 @@ public class ChristmasTree {
         //createTreeUp(10);
         //createTreeDown(10);
         //createTreeRight(10);
-        createTreeLeft(10);
+//        createTreeLeft(10);
+
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            l.add(i);
+        }
+        System.out.println(l);
+
+        Iterator<Integer> iterator = l.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(l.hashCode());
+
+        Set<Integer> s = new LinkedHashSet<>();
+        s.add(1);
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+
+        System.out.println(s);
+
+        Integer[] i = s.toArray(new Integer[s.size()]);
+
+        for (int i1: i
+             )
+            System.out.println(i1 + "<---");{
+
+        }
+
+        Map<Integer,String> myMap= new HashMap<>();
+
+        for (int j = 0; j < 10; j++) {
+            String k = String.valueOf(j);
+            myMap.put(j, k);
+        }
+
+        System.out.println(myMap);
+
+        for (int key: myMap.keySet()) {
+            String value = myMap.get(key);
+            System.out.println(key + " -> " + value);
+        }
+
+        for (Map.Entry<Integer, String> entries : myMap.entrySet()) {
+            Integer h = entries.getKey();
+            String k = entries.getValue();
+            System.out.println(h + " --> " + k);
+        }
+
     }
 
     public static void createTreeUp(int hight) {
@@ -66,5 +120,7 @@ public class ChristmasTree {
                 System.out.print("*");
             System.out.println();
         }
+
+
     }
 }
